f700624e36c29d0fdac7311807008777153b2180
Shruti Kamboj <kamboj.shruti01@gmail.com>
2025-04-15 17:26:54 +0530

    Update dashboardRoute.js

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const { dashboardDetails, dashboard } = require("../Controllers/dashboardController");
-const { auth, isAdmin, isReceptionist } = require("../middlewares/auth");
 const router = express.Router();
 
-router.get('/admin/dashboardDetails',  dashboardDetails)
+router.get('/admin/dashboardDetails', dashboardDetails)
 router.get('/receptionist/dashboardDetails',dashboard)
 
 module.exports = router;
